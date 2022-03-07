d74f2bbfcc45124d9214839adb88ca6d5770bce0
Jason Hernandez <kaltre10@gmail.com>
2022-03-07 12:30:48 -0500

    config port

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Server Active!!!'));
 
-// const port = process.env.PORT || 3000;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen( port, () => {
     console.log(`server activo en el puerto ${port}`);
