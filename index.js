409fe7fa4f00b25b34fc251674fa98a7294fd064
Jason Hernandez <kaltre10@gmail.com>
2022-03-08 11:33:43 -0500

    server dapp

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ routerApi(app);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/dapp', (req, res) => res.sendFile(path.join(__dirname, './dapp/index.html')));
 
 app.listen( port, () => {
     console.log(`server activo en el puerto ${port}`);
