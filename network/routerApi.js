7018695f7f41594186bfda1eb7dbfb2ddcfe07ee
Jason Hernandez <kaltre10@gmail.com>
2022-03-11 16:27:30 -0500

    config cors

--- network/routerApi.js
+++ network/routerApi.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const login = require('../components/login/network');
+const cors = require('cors');
 
 const routerApi = app => {
+    app.use(cors(), router);
     app.use('/api/v1', router);
     router.use('/login', login);
 }
