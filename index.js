0544fd5feed0f05629bdc658a6621fbc3fbf0240
Jason Hernandez <kaltre10@gmail.com>
2022-03-15 10:00:06 -0500

    router dapp, dasboard, market, shop, race

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ connect();
 
 routerApi(app);
 
-const port = process.env.PORT || 3000;
+    app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+    
+    app.get('/dapp', (req, res) => res.sendFile(path.join(__dirname, './dapp/index.html')));
+    app.get('/dashboard', (req, res) => res.sendFile(path.join(__dirname, './dapp/index.html')));
+    app.get('/market', (req, res) => res.sendFile(path.join(__dirname, './dapp/index.html')));
+    app.get('/shop', (req, res) => res.sendFile(path.join(__dirname, './dapp/index.html')));
+    app.get('/race', (req, res) => res.sendFile(path.join(__dirname, './dapp/index.html')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
-app.get('/dapp', (req, res) => res.sendFile(path.join(__dirname, './dapp/index.html')));
-app.get('*/', (req, res) => res.status(404).send('Error 404'));
+    app.get('*/', (req, res) => res.status(404).send('Error 404'));
+
+const port = process.env.PORT || 3000;
 
 app.listen( port, () => {
     console.log(`server activo en el puerto ${port}`);
