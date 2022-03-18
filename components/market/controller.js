fb6e1678ada3e83e46b73d86853006b1faf122d7
Jason Hernandez <kaltre10@gmail.com>
2022-03-18 15:14:12 -0500

    paginate data config

--- components/market/controller.js
+++ components/market/controller.js
@@ -5,7 +5,21 @@ const getAll = async (limit, page) => {
         try {
             const cansAll = await storeCans.getAll(limit, page);
             const { docs } = cansAll;
-            const cans = docs.filter( can => can.onSale.sale == true);
+            const cans = docs.filter(can => can.onSale.sale == true);
+
+            //paginate data
+            cans.push([{
+                totalDocs:cansAll.totalDocs,
+                limi: cansAll.limi,
+                totalPages: cansAll.totalPages,
+                page: cansAll.page,
+                pagingCounter: cansAll.pagingCounter,
+                hasPrevPage: cansAll.hasPrevPage,
+                hasNextPage: cansAll.hasNextPage,
+                prevPage: cansAll.prevPage,
+                nextPage: cansAll.nextPage
+            }]);
+           
             resolve(cans);
         } catch (error) {
             reject(error);
