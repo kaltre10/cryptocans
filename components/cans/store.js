fe64113df26edbf2a12261ef85cd5214da3ec5d7
Jason Hernandez <kaltre10@gmail.com>
2022-03-24 12:18:37 -0500

    all cans user

--- components/cans/store.js
+++ components/cans/store.js
@@ -4,7 +4,7 @@ const getAllCans = async () => await Model.find({ "onSale.sale": true, status: 1
 
 const getCans = id => Model.findOne({ id });
 
-const getCansUser = wallet => Model.find({ wallet, status: 1 });
+const getCansUser = wallet => Model.find({ wallet });
 
 const deleteCans = id => Model.deleteOne({ id });
 
