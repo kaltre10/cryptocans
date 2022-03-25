911fa3a12a600602f4ac763f2c668db28b9b8058
Jason Hernandez <kaltre10@gmail.com>
2022-03-25 13:31:24 -0500

    check canodromon add cans length

--- components/canodrome/controller.js
+++ components/canodrome/controller.js
@@ -1,5 +1,6 @@
 const store = require('./store');
 const storeCan = require('../user/store');
+const { exists } = require('./model');
 
 //GET CANODROMES USER
 const getAll = async (wallet) => {
@@ -46,13 +47,32 @@ const add = (wallet, type) => {
     });
 }
 
+//tipos de canodromos 
+const typeCanodrome = {
+    1: 3, //tipo 1 (common) cans max 3
+    2: 6, //tipo 2 (rares) cans max 6
+    3: 9, //tipo 3 (epic) cans max 9
+    4: 12 //tipo 4 (legendary) cans max 9
+}
+
+const checkAddCan = async (id) => {
+    const canodrome = await get(id);
+    if(canodrome.cans.length > typeCanodrome[canodrome.type] -1) return false;
+    
+    return true;
+}
+
 //UPDATE CANODROME
 const update = (id, can) => {
     return new Promise( async (resolve, reject) => {
         try {
-            // const cans = [can];
+            const checkLength = await checkAddCan(id);
+           
+            if(!checkLength) throw 'Este canodromo no tiene suficiente energia';
+
             const canodrome = await store.update(id, can);
             resolve(canodrome);
+
         } catch (error) {
             reject(error);
         }
