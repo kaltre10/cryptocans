c1516c6adec132d54f98e88b564d24d4c03b960e
Jason Hernandez <kaltre10@gmail.com>
2022-04-02 21:10:04 -0500

    error login

--- components/user/store.js
+++ components/user/store.js
@@ -1,7 +1,7 @@
 const Model = require('./model');
 
-const getUser = async wallet => Model.findOne({ wallet }).exec();
-const addUser = async wallet => Model.create({wallet}).exec();
+const getUser = async wallet => Model.findOne({ wallet }).exec();;
+const addUser = async wallet => Model({wallet}).save();
 const updateUser = ({ wallet, balanceAfter}) => Model.findOneAndUpdate({ wallet }, { balance: balanceAfter}, { new: true})
 
 module.exports = {
