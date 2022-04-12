83a38c1b9710aed62bed772173d35c2eff22692c
Jason Hernandez <kaltre10@gmail.com>
2022-04-11 22:47:59 -0500

    env cross-env

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 require('dotenv').config({ path: '.env' });
 const mongoose = require("mongoose");
 
+const { CONNECT_PRODUCTION, CONNECT_DEVELOPMENT, NODE_ENV } = process.env;
+
+const connectString = NODE_ENV === 'development' ? CONNECT_DEVELOPMENT : CONNECT_PRODUCTION;
+
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.CONNECT);
+        await mongoose.connect(connectString);
         console.log('conectado mongoDB');
     } catch (error) {
         console.log(error);
